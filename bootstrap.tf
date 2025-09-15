9e6e3f8a59bd93948d5986d47e12c5f3032e301e
lavlinskiy <alexanderlavlinskiy@gmail.com>
2025-09-15 18:34:37 +0300

    commit1

--- bootstrap.tf
+++ bootstrap.tf
@@ -4,7 +4,7 @@ resource "random_id" "bucket_suffix" {
 
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "lavltfstatebucket-${random_id.bucket_suffix.hex}"
-
+  force_destroy = true
   tags = {
     Name = "Terraform State Storage"
   }
