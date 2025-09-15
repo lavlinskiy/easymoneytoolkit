57ba1dacc1cea28cd5184a2cee3c4035086986f2
lavlinskiy <alexanderlavlinskiy@gmail.com>
2025-09-15 23:27:18 +0300

    Commit 10

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -1,4 +1,3 @@
-# Создаём ключ EC2 из статичного public_key
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
   public_key = var.public_key
@@ -9,35 +8,25 @@ resource "aws_key_pair" "this" {
   public_key = var.ec2_ssh_public_key
 }
 
-# EC2 инстанс
 resource "aws_instance" "app_server" {
   ami                         = var.ami
   instance_type               = var.instance_type
   subnet_id                   = var.subnet_id
   vpc_security_group_ids      = var.security_group_ids
   key_name                    = aws_key_pair.this.key_name
-  associate_public_ip_address = false  # IP через Elastic IP
+  associate_public_ip_address = false
   user_data                   = file("${path.module}/../../user_data/init_ec2.sh")
 
-  tags = {
-    Name = "PHP-Nginx-ELK-Grafana"
-  }
+  tags = { Name = "PHP-Nginx-ELK-Grafana" }
 }
 
-# Elastic IP
 resource "aws_eip" "app_server_eip" {
-  vpc = true
+  instance = aws_instance.app_server.id
+  domain   = "vpc"
 }
 
-# Ассоциация Elastic IP с EC2
-resource "aws_eip_association" "app_server_assoc" {
-  instance_id   = aws_instance.app_server.id
-  allocation_id = aws_eip.app_server_eip.id
-}
-
-# Provisioners для копирования файлов внутрь EC2 через Elastic IP
 resource "null_resource" "provision_files" {
-  depends_on = [aws_eip_association.app_server_assoc]
+  depends_on = [aws_eip.app_server_eip]
 
   provisioner "file" {
     source      = "${path.module}/../../app/index.php"
--- modules/ec2_instance/outputs.tf
+++ modules/ec2_instance/outputs.tf
@@ -1,34 +1,27 @@
 output "instance_public_ip" {
-  description = "Elastic IP address of the EC2 instance"
-  value       = aws_eip.app_server_eip.public_ip
+  value = aws_eip.app_server_eip.public_ip
 }
 
-output "instance_dns_name" {
-  description = "Public DNS of the EC2 instance (assigned AWS)"
-  value       = aws_instance.app_server.public_dns
+output "instance_public_dns" {
+  value = aws_instance.app_server.public_dns
 }
 
 output "php_app_url" {
-  description = "URL для PHP-приложения"
-  value       = "http://${aws_instance.app_server.public_dns}/"
+  value = "https://${aws_instance.app_server.public_dns}/"
 }
 
 output "grafana_url" {
-  description = "URL для Grafana"
-  value       = "http://${aws_instance.app_server.public_dns}:3000"
+  value = "https://${aws_instance.app_server.public_dns}:3000"
 }
 
 output "kibana_url" {
-  description = "URL для Kibana"
-  value       = "http://${aws_instance.app_server.public_dns}:5601"
+  value = "https://${aws_instance.app_server.public_dns}:5601"
 }
 
 output "logstash_url" {
-  description = "URL для Logstash"
-  value       = "http://${aws_instance.app_server.public_dns}:5044"
+  value = "https://${aws_instance.app_server.public_dns}:5044"
 }
 
 output "elasticsearch_url" {
-  description = "URL для Elasticsearch"
-  value       = "http://${aws_instance.app_server.public_dns}:9200"
+  value = "https://${aws_instance.app_server.public_dns}:9200"
 }
